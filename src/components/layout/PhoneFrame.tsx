a9e542dc49ba6b74748cbe76db44edac9e57609a
BIPLABx019 <biplabx019@gmail.com>
2025-09-17 08:23:33 +0530

    add mobile web support

--- src/components/layout/PhoneFrame.tsx
+++ src/components/layout/PhoneFrame.tsx
@@ -7,11 +7,12 @@ interface PhoneFrameProps {
 
 const PhoneFrame: React.FC<PhoneFrameProps> = ({ children }) => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="
-        w-[360px] 
-        h-[90vh] max-h-[700px]
-        bg-white rounded-[2.5rem] shadow-xl border-4 border-gray-300 
+      h-[100vh] w-full
+        sm:w-[360px] 
+        sm:h-[90vh] sm:max-h-[700px]
+        bg-white sm:rounded-[2.5rem] shadow-xl border-4 border-gray-300 
         relative overflow-hidden
       ">
         <div className="h-full overflow-y-auto no-scrollbar">{children}</div>
