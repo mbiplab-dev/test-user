a0562dda26c2664f5e569137be8b5f113ccc12ca
BIPLABx019 <biplabx019@gmail.com>
2025-09-17 08:30:19 +0530

    .

--- src/components/layout/PhoneFrame.tsx
+++ src/components/layout/PhoneFrame.tsx
@@ -8,13 +8,15 @@ interface PhoneFrameProps {
 const PhoneFrame: React.FC<PhoneFrameProps> = ({ children }) => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="
-      h-[100vh] w-full
+      <div
+        className="
+        h-[100vh] w-full max-h-screen
         sm:w-[360px] 
         sm:h-[90vh] sm:max-h-[700px]
         bg-white sm:rounded-[2.5rem] shadow-xl border-4 border-gray-300 
         relative overflow-hidden
-      ">
+      "
+      >
         <div className="h-full overflow-y-auto no-scrollbar">{children}</div>
       </div>
     </div>
